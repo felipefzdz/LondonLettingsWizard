2e7c03bf4ef78f0c74cc9e6bf5b91e97cb16504c
Felipe Fernandez <olid16@gmail.com>
2013-09-03 15:58:00 +0100

    Adding authentication

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,20 +1,42 @@
 package controllers;
 
 import models.Area;
-import play.*;
+import models.User;
 import play.mvc.*;
 import play.data.*;
 
 import views.html.*;
 
+import static play.data.Form.form;
+
 public class Application extends Controller {
 
 
-    static Form<Area> areaForm = Form.form(Area.class);
+    static Form<Area> areaForm = form(Area.class);
 
     public static Result index() {
         return redirect(routes.Application.areas());
     }
+
+    public static Result login() {
+        return ok(
+                login.render(form(Login.class))
+        );
+    }
+
+    public static Result authenticate() {
+        Form<Login> loginForm = form(Login.class).bindFromRequest();
+        if (loginForm.hasErrors()) {
+            return badRequest(login.render(loginForm));
+        } else {
+            session().clear();
+            session("email", loginForm.get().email);
+            return redirect(
+                    routes.Application.index()
+            );
+        }
+    }
+
 
     public static Result areas() {
         return ok(
@@ -37,6 +59,22 @@ public class Application extends Controller {
     public static Result deleteArea(Long id) {
         Area.delete(id);
         return redirect(routes.Application.areas());
+    }
+
+
+    public static class Login {
+
+        public String email;
+        public String password;
+
+
+        public String validate() {
+            if (User.authenticate(email, password) == null) {
+                return "Invalid user or password";
+            }
+            return null;
+        }
+
     }
 
 
